02181fd7a4cd639068ad6bd0e76add77c3b95022
Steinunn <stf7@hi.is>
2015-01-29 12:15:04 +0000

    New Corners

---
+++ Corners/android/src/com/corners/game/android/AndroidLauncher.java
@@ -0,0 +1,16 @@
+package com.corners.game.android;
+
+import android.os.Bundle;
+
+import com.badlogic.gdx.backends.android.AndroidApplication;
+import com.badlogic.gdx.backends.android.AndroidApplicationConfiguration;
+import com.corners.game.MainActivity;
+
+public class AndroidLauncher extends AndroidApplication {
+	@Override
+	protected void onCreate (Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		AndroidApplicationConfiguration config = new AndroidApplicationConfiguration();
+		initialize(new MainActivity(), config);
+	}
+}
