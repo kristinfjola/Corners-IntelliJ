86fe1fc94174d8c6bba5aa5cc0b803f7e363ce0b
eddabjork <ebk13@hi.is>
2015-03-26 11:32:38 +0000

    höndla ekki loggaður inn á fb

--- Corners/core/src/screens/Friends.java
+++ Corners/core/src/screens/Friends.java
@@ -45,6 +45,7 @@ public class Friends implements Screen{
 	TextButton btnTest;
 	List<String> friends;
 	LabelStyle friendsStyle;
+	LabelStyle friendsStyleM;
 	
 	private InputProcessor inputProcessor;
 	Table table;
@@ -63,7 +64,8 @@ public class Friends implements Screen{
 		Gdx.input.setInputProcessor(stage);
 		addBackToProcessor();
 		setAllProcessors();
-		friends = main.facebookService.getFriendsList();
+		friends = main.facebookService.isLoggedIn() ? main.facebookService.getFriendsList() : new ArrayList<String>();
+		friendsStyleM = new LabelStyle(main.skin.getFont(main.screenSizeGroup+"-M"), Color.BLACK);
 		friendsStyle = new LabelStyle(main.skin.getFont(main.screenSizeGroup+"-S"), Color.BLACK);
 	}
 	
@@ -76,58 +78,66 @@ public class Friends implements Screen{
 		
 		setUpInfoBar();
 		
-		//list of scores from friends
-		List<Integer> scores = main.facebookService.getScores();
-		Integer my_score_temp = main.facebookService.getMyScore();
-		HashMap<Integer, String> friends_hash_temp = new HashMap<Integer, String>();
-		
-		for(int i = 0; i< scores.size(); i++) {
-			friends_hash_temp.put(scores.get(i), friends.get(i));
-		}
-		//add user to high score list
-		friends_hash_temp.put(my_score_temp, main.facebookService.getUserName());
-		
-		List keys = new ArrayList(friends_hash_temp.keySet());
-		Collections.sort(keys);
-		Collections.reverse(keys);
-		
-		Map<Integer, String> friends_hash = new TreeMap(Collections.reverseOrder());
-		friends_hash.putAll(friends_hash_temp);
-		
-		double stars = 0;
-		int finished_levels = 0;
-		
-		String stars_image;
-		
-		//TODO: refactora
-		
-		String my_score = Integer.toString(my_score_temp);
-		
-		int i = 0;
-		
-		for (Integer key : friends_hash.keySet()) {
-			String score = Integer.toString(key);
-			if(!score.equals("-1")) { //if permission to see score
-				if(score.length() == 4) {
-					stars = Double.parseDouble(score.substring(0, 3));
-					stars = stars / 100;
-					finished_levels = Integer.parseInt(score.substring(3));
-				} else if(score.length() == 3) {
-					stars = Double.parseDouble(score.substring(0, 2));
-					stars = stars / 10;
-					finished_levels = Integer.parseInt(score.substring(2));
-				} else {
-					stars = Double.parseDouble(score.substring(0, 1));
-					finished_levels = Integer.parseInt(score.substring(1));
+		if(main.facebookService.isLoggedIn()){
+			//list of scores from friends
+			List<Integer> scores = main.facebookService.getScores();
+			Integer my_score_temp = main.facebookService.getMyScore();
+			HashMap<Integer, String> friends_hash_temp = new HashMap<Integer, String>();
+			
+			for(int i = 0; i< scores.size(); i++) {
+				friends_hash_temp.put(scores.get(i), friends.get(i));
+			}
+			//add user to high score list
+			friends_hash_temp.put(my_score_temp, main.facebookService.getUserName());
+			
+			List keys = new ArrayList(friends_hash_temp.keySet());
+			Collections.sort(keys);
+			Collections.reverse(keys);
+			
+			Map<Integer, String> friends_hash = new TreeMap(Collections.reverseOrder());
+			friends_hash.putAll(friends_hash_temp);
+			
+			double stars = 0;
+			int finished_levels = 0;
+			
+			String stars_image;
+			
+			//TODO: refactora
+			
+			String my_score = Integer.toString(my_score_temp);
+			
+			int i = 0;
+			
+			for (Integer key : friends_hash.keySet()) {
+				String score = Integer.toString(key);
+				if(!score.equals("-1")) { //if permission to see score
+					if(score.length() == 4) {
+						stars = Double.parseDouble(score.substring(0, 3));
+						stars = stars / 100;
+						finished_levels = Integer.parseInt(score.substring(3));
+					} else if(score.length() == 3) {
+						stars = Double.parseDouble(score.substring(0, 2));
+						stars = stars / 10;
+						finished_levels = Integer.parseInt(score.substring(2));
+					} else {
+						stars = Double.parseDouble(score.substring(0, 1));
+						finished_levels = Integer.parseInt(score.substring(1));
+					}
 				}
+				stars_image = "infoBar/"+infoBar.getStarAmount(stars)+".png";
+				if(key != -1) {
+					table.add(new Label(""+(i+1)+". "+friends_hash.get(key)+"  ("+stars+"/"+finished_levels+")", friendsStyle)).left().padLeft(main.scrWidth/24f);
+					table.add(new Image(new Texture(stars_image))).size(main.scrWidth/6).right().padRight(main.scrWidth/26f).row();
+				}
+				i++;
 			}
-			stars_image = "infoBar/"+infoBar.getStarAmount(stars)+".png";
-			if(key != -1) {
-				table.add(new Label(""+(i+1)+". "+friends_hash.get(key)+"  ("+stars+"/"+finished_levels+")", friendsStyle)).left().padLeft(main.scrWidth/24f);
-				table.add(new Image(new Texture(stars_image))).size(main.scrWidth/6).right().padRight(main.scrWidth/26f).row();
-			}
-			i++;
+		} else {
+			table.add(new Label("Oops! You're not logged into",friendsStyleM)).left().padTop(main.scrWidth/18f).padLeft(main.scrWidth/24f).row();
+			table.add(new Label("facebook!",friendsStyleM)).left().padLeft(main.scrWidth/24f).row();
+			table.add(new Label("Go to Settings on the start screen and log", friendsStyle)).left().padLeft(main.scrWidth/24f).padTop(main.scrWidth/24f).row();
+			table.add(new Label("into facebook to see your friends!", friendsStyle)).left().padLeft(main.scrWidth/24f).row();
 		}
+		
 	}
 
 	/**
