fd58d374b28093b871f1b80a8397d2e2e0d55c5f
Kristin Fjola <kristinfjolato@gmail.com>
2015-04-09 16:22:22 +0000

    Notifications fix

--- Corners/android/src/com/corners/game/android/NotificationsImpl.java
+++ Corners/android/src/com/corners/game/android/NotificationsImpl.java
@@ -83,9 +83,13 @@ public class NotificationsImpl extends BroadcastReceiver implements Notification
 	     // repeat at 15:00 every day
 	     Calendar calendar = Calendar.getInstance();
 	     calendar.setTimeInMillis(System.currentTimeMillis());
-	     calendar.add(Calendar.DATE, 1);
+	     
 	     calendar.set(Calendar.HOUR_OF_DAY, 15);
 	     calendar.set(Calendar.MINUTE, 0);
+	     
+	     if(System.currentTimeMillis() > calendar.getTimeInMillis()){
+	    	 calendar.add(Calendar.DATE, 1);
+	     }
 
 	     am.setInexactRepeating(AlarmManager.RTC_WAKEUP, calendar.getTimeInMillis(), AlarmManager.INTERVAL_DAY, pi);
 	     //am.setRepeating(AlarmManager.RTC_WAKEUP, System.currentTimeMillis(), 1000 * 10, pi);
